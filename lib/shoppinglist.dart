9d51174144e87b38bd2b0402b34087ff1c4714b9
Markus Glas <markus.glas@gmx.net>
2021-01-21 17:23:00 +0100

    add bg image and change font style and size

--- lib/shoppinglist.dart
+++ lib/shoppinglist.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'listitem.dart';
@@ -16,9 +17,11 @@ class ShoppingList extends StatefulWidget {
 }
 
 class _ShoppingListState extends State<ShoppingList> {
+  static const double _SMALL_FONT_SIZE = 28;
+  static const double _LARGE_FONT_SIZE = 40;
   List<ListItem> _itemList = [];
   TextEditingController inputController = new TextEditingController();
-  double _fontSize = 22;
+  double _fontSize = _LARGE_FONT_SIZE;
 
   @override
   void initState() {
@@ -68,7 +71,8 @@ class _ShoppingListState extends State<ShoppingList> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(decoration: BoxDecoration(image: DecorationImage(image: new AssetImage("images/ruled_paper.png"), fit: BoxFit.cover)),child: Scaffold(
+      backgroundColor: Colors.transparent,
       appBar: AppBar(
         title: Text(widget.title),
         // backgroundColor: Colors.lightGreen,
@@ -77,14 +81,14 @@ class _ShoppingListState extends State<ShoppingList> {
             key: Key("font_size"),
             icon: Icon(Icons.format_size),
             onPressed: () => setState(() {
-              _fontSize==22 ? _fontSize=28 : _fontSize=22;
+              _fontSize==_LARGE_FONT_SIZE ? _fontSize=_SMALL_FONT_SIZE : _fontSize=_LARGE_FONT_SIZE;
             }),
           ),
-          IconButton(
-            key: Key("set_complete"),
-            icon: Icon(Icons.check),
-            onPressed: () => _showDialogForCompletingAllItems(context),
-          ),
+          // IconButton(
+          //   key: Key("set_complete"),
+          //   icon: Icon(Icons.check),
+          //   onPressed: () => _showDialogForCompletingAllItems(context),
+          // ),
           IconButton(
               key: Key("delete_completed"),
               icon: Icon(Icons.remove_done),
@@ -94,6 +98,7 @@ class _ShoppingListState extends State<ShoppingList> {
           ),
           IconButton(
               key: Key("delete_all"),
+              color: Colors.redAccent,
               icon: Icon(Icons.delete_forever_outlined),
               onPressed: () => _itemList.length > 0
                   ? _showAlertDialogForDeletingAllItems(context)
@@ -113,7 +118,7 @@ class _ShoppingListState extends State<ShoppingList> {
         onPressed: () =>  showDialogForAddingItems(context),
         child: Icon(Icons.add, color: Colors.white),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 
   void showDialogForAddingItems(BuildContext context) {
@@ -154,56 +159,11 @@ class _ShoppingListState extends State<ShoppingList> {
         });
   }
 
-  void _showDialogForCompletingAllItems(BuildContext context) {
-     showDialog(
-        context: context,
-        builder: (BuildContext buildContext) {
-          return AlertDialog(
-            key: Key("delete_completed_dialog"),
-            title: new Text("Alle Artikel als erledigt markieren?"),
-            actions: <Widget>[
-              new FlatButton(
-                  key: Key("confirm"),
-                  onPressed: () {
-                    _completeAllItems();
-                    Navigator.of(context).pop();
-                  },
-                  child: new Text('Ja')
-              ),
-              new FlatButton(
-                  key: Key("deny"),
-                  onPressed: Navigator.of(context).pop,
-                  child: new Text('Nein')
-              )
-            ],
-          );
-        }
-    );
-  }
-
   void _showDialogForDeletingCompletedItems(BuildContext context) {
      showDialog(
         context: context,
         builder: (BuildContext buildContext) {
-          return AlertDialog(
-            key: Key("delete_completed_dialog"),
-            title: new Text("Alle erledigten Artikel löschen?"),
-            actions: <Widget>[
-              new FlatButton(
-                  key: Key("confirm"),
-                  onPressed: () {
-                    _clearCompletedItems();
-                    Navigator.of(context).pop();
-                  },
-                  child: new Text('Ja')
-              ),
-              new FlatButton(
-                  key: Key("deny"),
-                  onPressed: Navigator.of(context).pop,
-                  child: new Text('Nein')
-              )
-            ],
-          );
+          return showAlertDialog(context, "Alle erledigten Artikel löschen?", _clearCompletedItems);
         }
     );
   }
@@ -212,26 +172,30 @@ class _ShoppingListState extends State<ShoppingList> {
      showDialog(
         context: context,
         builder: (BuildContext buildContext) {
-          return AlertDialog(
-            key: Key("delete_all_dialog"),
-            title: new Text("Liste leeren?"),
-            actions: <Widget>[
-              new FlatButton(
-                  key: Key("confirm"),
-                  onPressed: () {
-                    _clearAllItems();
-                    Navigator.of(context).pop();
-                  },
-                  child: new Text('Ja')
-              ),
-              new FlatButton(
-                  key: Key("deny"),
-                  onPressed: Navigator.of(context).pop,
-                  child: new Text('Nein')
-              )
-            ],
-          );
+          return showAlertDialog(context, "Liste leeren?", _clearAllItems);
         }
+    );
+  }
+
+  AlertDialog showAlertDialog(BuildContext context, String dialogTitle, Function() methodToCall) {
+    return AlertDialog(
+          key: Key("delete_all_dialog"),
+      title: new Text(dialogTitle),
+      actions: <Widget>[
+        new FlatButton(
+            key: Key("confirm"),
+            onPressed: () {
+              methodToCall();
+              Navigator.of(context).pop();
+            },
+            child: new Text('Ja')
+        ),
+        new FlatButton(
+            key: Key("deny"),
+            onPressed: Navigator.of(context).pop,
+            child: new Text('Nein')
+        )
+      ],
     );
   }
 
@@ -247,11 +211,13 @@ class _ShoppingListState extends State<ShoppingList> {
     return new ListTile(
         title: new Text(
             text,
-            style: TextStyle(
-                color: _itemList[itemIndex].isChecked ? Colors.black12 : Colors.black,
+            style: GoogleFonts.caveat(
+                textStyle: TextStyle(
+                color: _itemList[itemIndex].isChecked ? Colors.black26 : Colors.black,
                 fontSize: _fontSize,
-                decoration: _itemList[itemIndex].isChecked ? TextDecoration.lineThrough : TextDecoration.none)),
-        tileColor: Colors.black12,
+                decoration: _itemList[itemIndex].isChecked ? TextDecoration.lineThrough : TextDecoration.none))
+            ),
+        tileColor: Colors.transparent,
         onTap: () => setState(() {
           _itemList[itemIndex].isChecked = !_itemList[itemIndex].isChecked;
           _saveData();
